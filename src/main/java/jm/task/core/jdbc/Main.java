e6ce188bd77d101024aa544b1cd26ac1f036061d
sosmy <akynbekov.aibek1580@gmail.com>
2023-09-29 21:19:14 +0300

    jdbc

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import org.hibernate.collection.internal.PersistentBag;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +17,10 @@ public class Main {
         userService.saveUser("Test1", "Test2", (byte) 24);
         userService.saveUser("Test1", "Test2", (byte) 25);
         userService.saveUser("Test1", "Test2", (byte) 26);
-        userService.getAllUsers();
+        List<User> users = userService.getAllUsers();
+        for (User user: users) {
+            System.out.println(user);
+        }
         userService.cleanUsersTable();
         userService.removeUserById(1);
     }
